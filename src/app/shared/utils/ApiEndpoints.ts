30fe20b5cb4eb6ee1f0ecdc72882875ab9782f4e
rahneverd <rahneverd@gmail.com>
2024-11-03 16:10:04 +0500

    working on implementing http interceptor

--- src/app/shared/utils/ApiEndpoints.ts
+++ src/app/shared/utils/ApiEndpoints.ts
@@ -3,5 +3,7 @@ import { environment } from 'src/environments/environment';
 export const API_BASE_URL: string = environment.baseUrl;
 
 export enum API_ENDPOINTS {
+  // users routes
   LOGIN = 'user/login',
+  FIND_ALL = 'user/findAll',
 }
--- src/app/ums/components/users/users-list/users-list.component.ts
+++ src/app/ums/components/users/users-list/users-list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/shared/services/api.service';
+import { API_ENDPOINTS } from 'src/app/shared/utils/ApiEndpoints';
+import { API_STATUS_CODE } from 'src/app/shared/utils/Constants';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent {
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit() {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.apiService
+      .call({}, {}, API_ENDPOINTS.FIND_ALL, true)
+      .subscribe((resp) => {
+        if (resp.statusCode === API_STATUS_CODE.OK) {
+          console.log(resp);
+        } else {
+          // this.alertService.showErrorAlert(resp?.message);
+        }
+      });
+  }
 }
