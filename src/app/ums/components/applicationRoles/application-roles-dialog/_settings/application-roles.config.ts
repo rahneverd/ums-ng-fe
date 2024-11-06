91d8b2b5fa17cd6cf5283ab7fe0c1fc36c736172
Muhammad Owais <rahneverd@gmail.com>
2024-11-06 16:30:34 +0500

    application roles dialog updated

---
+++ src/app/ums/components/applicationRoles/application-roles-dialog/_settings/application-roles.config.ts
@@ -0,0 +1,114 @@
+import { Validators } from '@angular/forms';
+import { API_ENDPOINTS } from 'src/app/shared/utils/ApiEndpoints';
+import { ACTIONS, CONST, FORM_CONSTANTS } from 'src/app/shared/utils/Constants';
+
+const moduleName = 'Application Roles';
+
+// FORM CONFIGURATION
+
+export enum fieldLabels {
+  title = 'Title',
+  description = 'Description',
+  applicationId = 'applicationId',
+  lovStatusId = 'Status',
+}
+
+export enum controlNames {
+  title = 'title',
+  description = 'description',
+  applicationId = 'applicationId',
+  lovStatusId = 'lovStatusId',
+}
+
+export const APPLICATION_ROLES_FORM_CONFIG = {
+  formTitle: moduleName,
+  formClass: 'card p-fluid',
+  controlsClass: 'row mb-3',
+  formControls: [
+    {
+      fieldId: FORM_CONSTANTS.FIELD_TEXT_ID,
+      controlId: FORM_CONSTANTS.CTRL_TEXT_BOX_ID,
+      fieldLabel: fieldLabels.title,
+      controlName: controlNames.title,
+      // nestedPath?: string;
+      // onChange?: boolean;
+      showIcon: true,
+      outerClass: 'col-md-4 form-field-container',
+      labelClass: 'form-field-label',
+      spanClass: 'p-input-icon-right',
+      iconClass: 'pi pi-user',
+      validations: [Validators.required],
+      // fieldClass: 'input-text',
+      // id: string
+    },
+    {
+      fieldId: FORM_CONSTANTS.FIELD_TEXT_ID,
+      controlId: FORM_CONSTANTS.CTRL_TEXT_BOX_ID,
+      fieldLabel: fieldLabels.description,
+      controlName: controlNames.description,
+      // nestedPath?: string;
+      // onChange?: boolean;
+      showIcon: true,
+      outerClass: 'col-md-4 form-field-container',
+      labelClass: 'form-field-label',
+      spanClass: 'p-input-icon-right',
+      iconClass: 'pi pi-user',
+      validations: [Validators.required],
+      // fieldClass: 'input-text',
+      // id: string
+    },
+    {
+      fieldId: FORM_CONSTANTS.FIELD_TEXT_ID,
+      controlId: FORM_CONSTANTS.CTRL_DROP_DOWN_ID,
+      fieldLabel: fieldLabels.applicationId,
+      controlName: controlNames.applicationId,
+      // nestedPath?: string;
+      // onChange?: boolean;
+      showIcon: true,
+      outerClass: 'col-md-4 form-field-container',
+      labelClass: 'form-field-label',
+      spanClass: 'p-input-icon-right',
+      iconClass: 'pi pi-user',
+      validations: [Validators.required],
+      // fieldClass: 'input-text',
+      // id: string
+    },
+    {
+      fieldId: FORM_CONSTANTS.FIELD_TEXT_ID,
+      controlId: FORM_CONSTANTS.CTRL_RADIO_ID,
+      fieldLabel: fieldLabels.lovStatusId,
+      controlName: controlNames.lovStatusId,
+      valuesList: [
+        { key: 'active', value: CONST.LID_ACTIVE_ID, label: 'Active' },
+        { key: 'inactive', value: CONST.LID_INACTIVE_ID, label: 'In Active' },
+      ],
+      // nestedPath?: string;
+      // onChange?: boolean;
+      showIcon: true,
+      outerClass: 'col-md-4 form-field-container',
+      labelClass: 'form-field-label',
+      spanClass: 'p-input-icon-right',
+      iconClass: 'pi pi-user',
+      validations: [Validators.required],
+      // fieldClass: 'input-text',
+      // id: string
+    },
+  ],
+  formActions: [
+    {
+      actionName: ACTIONS.ADD,
+      actionTitle: ACTIONS.ADD_TITLE,
+      buttonSelector: FORM_CONSTANTS.SELECTOR_BUTTON,
+      validations: [
+        controlNames.title,
+        controlNames.description,
+        controlNames.lovStatusId,
+      ],
+      actionUrl: API_ENDPOINTS.APPLICATION_ROLES_CREATE,
+      showToast: true,
+    },
+  ],
+  formData: {},
+
+  pagination: {},
+};
--- src/app/ums/components/applicationRoles/application-roles-dialog/application-roles-dialog.component.ts
+++ src/app/ums/components/applicationRoles/application-roles-dialog/application-roles-dialog.component.ts
@@ -1,10 +1,77 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FormConfig } from 'src/app/shared/models/form.model';
+import { ApiService } from 'src/app/shared/services/api.service';
+import { APPLICATION_ROLES_FORM_CONFIG } from './_settings/application-roles.config';
+import { ACTIONS, API_STATUS_CODE } from 'src/app/shared/utils/Constants';
+import { API_ENDPOINTS } from 'src/app/shared/utils/ApiEndpoints';
+import { ApiResponse } from 'src/app/shared/models/api.model';
 
 @Component({
   selector: 'app-application-roles-dialog',
   templateUrl: './application-roles-dialog.component.html',
-  styleUrls: ['./application-roles-dialog.component.scss']
+  styleUrls: ['./application-roles-dialog.component.scss'],
 })
 export class ApplicationRolesDialogComponent {
+  subscription: Subscription = new Subscription();
+  formConfig: FormConfig;
 
+  applicationsList: any[] = [];
+
+  // private alertService: AlertService
+  // private loginService: LoginService,
+  constructor(private apiService: ApiService) {
+    this.formConfig = new FormConfig(APPLICATION_ROLES_FORM_CONFIG);
+  }
+
+  ngOnInit() {
+    this.getAllApplications();
+  }
+
+  getAllApplications() {
+    this.subscription = this.apiService
+      .call({}, {}, API_ENDPOINTS.APPLICATION_FIND_ALL, false)
+      .subscribe((resp: any) => {
+        if (resp.statusCode === API_STATUS_CODE.OK) {
+          // this.loginService.login(ApiResponse.getData(resp));
+          this.applicationsList = ApiResponse.getData(resp);
+          console.log(this.applicationsList);
+        } else {
+          // this.alertService.showErrorAlert(resp?.message);
+        }
+      });
+  }
+
+  onAction(event: any) {
+    console.log(event);
+    if (event?.action?.actionName === ACTIONS.ADD) {
+      this.add(event);
+    } else if (event?.action?.actionName === ACTIONS.UPDATE) {
+      // this.update()
+    } else if (event?.action?.actionName === ACTIONS.DELETE) {
+      // this.delete()
+    }
+    // if (event?.action?.actionName === ACTIONS.LOGIN) {
+    //   this.login(event);
+    // } else if (event?.action?.actionName === ACTIONS.REGISTER) {
+    //   this.router.navigate([FE_URLS.REGISTER]);
+    // }
+  }
+
+  add(obj: any) {
+    console.log(obj);
+    this.subscription = this.apiService
+      .call(obj?.data, {}, obj.action?.actionUrl, obj.action.show)
+      .subscribe((resp: any) => {
+        if (resp.statusCode === API_STATUS_CODE.OK) {
+          // this.loginService.login(ApiResponse.getData(resp));
+        } else {
+          // this.alertService.showErrorAlert(resp?.message);
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
