dacb189aed43df076599688005fe4740d4b789fc
Muhammad Owais <rahneverd@gmail.com>
2024-11-07 11:45:08 +0500

    ngx ui loader added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,48 @@ import { TopBarComponent } from './layout/top-bar/top-bar.component';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { MenuItemComponent } from './layout/menu-item/menu-item.component';
+import {
+  NgxUiLoaderConfig,
+  NgxUiLoaderHttpModule,
+  NgxUiLoaderModule,
+  NgxUiLoaderRouterModule,
+  PB_DIRECTION,
+  SPINNER,
+} from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  fgsColor: '#dee2e6',
+  pbColor: '#dee2e6',
+  bgsOpacity: 0.1,
+  fgsType: SPINNER.threeBounce,
+  pbDirection: PB_DIRECTION.leftToRight,
+  pbThickness: 3,
+  // "minTime": 3000,
+  overlayColor: 'rgba(40, 40, 40, 0.1)',
+};
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, TopBarComponent, LayoutComponent, MenuItemComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    TopBarComponent,
+    LayoutComponent,
+    MenuItemComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
+    NgxUiLoaderRouterModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
+    NgxUiLoaderHttpModule.forRoot({
+      showForeground: true,
+      // excludeRegexp: [
+      // '',
+      // ],
+    }),
   ],
   providers: [
     {
